3e4022f58a4a7f0efda5c89c12ba27ff3736015a
Vanik Gevorgyan <vanik08@gmail.com>
2018-03-11 00:43:44 -0800

    Postit styles with position

--- src/components/Postit.js
+++ src/components/Postit.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Paper from 'material-ui/Paper';
+
+import './Postit.css';
 
 class Postit extends Component {
   static propTypes = {
     id: PropTypes.string.isRequired,
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired,
   };
 
   render() {
-    return <div className="Postit">postit {this.props.id}</div>;
+    return (
+      <Paper
+        className="Postit"
+        style={{
+          position: 'absolute',
+          top: 50,
+          left: 0,
+        }}
+      >
+        {this.props.id}
+      </Paper>
+    );
   }
 }
 
